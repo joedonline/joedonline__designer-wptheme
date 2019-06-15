a9845334f9665298d5f825b3c8e9ecc75bf098f2
joedonline <contact@joedomingo.online>
2019-06-15 19:12:11 -0400

    Header categories updated, aria-current implemented with termSlugHelper function.

--- src/_wptheme/template-parts/content__header-categories.php
+++ src/_wptheme/template-parts/content__header-categories.php
@@ -18,12 +18,21 @@
     );
 
     $categories = get_categories( $args );
+    $term = get_queried_object();
+
+    function termSlugHelper( $category, $term ) {
+      if ( $category->term_id == $term->term_id ) {
+        return $term->slug;
+      }
+      return -1;
+    }
 
     foreach ( $categories as $category ) {
-      printf( '<li class="cat-item"><a href="%1$s">%2$s</a>&nbsp;(%3$s)</li>',
+      printf( '<li class="cat-item"><a href="%1$s" aria-current="%4$s">%2$s</a>&nbsp;&nbsp;<sub>%3$s</sub></li>',
           esc_url( get_category_link( $category->term_id ) ),
           esc_html( $category->name ),
-          esc_html( $category->count )
+          esc_html( $category->count ),
+          esc_html( termSlugHelper( $category, $term ) )
       );
     }
   ?>
