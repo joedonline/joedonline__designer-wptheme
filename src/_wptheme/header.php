61237435b014cd73751486e4a05a26851e38847e
joedonline <contact@joedomingo.online>
2019-06-16 20:32:01 -0400

    clean code

--- src/_wptheme/header.php
+++ src/_wptheme/header.php
@@ -14,7 +14,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<link rel="shortcut icon" href="<?php echo get_template_directory_uri() . "/assets/favicon.ico"; ?>" ?>" />
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri() . "/assets/favicon.ico"; ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
